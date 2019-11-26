3c0cc99001c568ce645dbc38c78a8239ba41c3f0
Joel  Bly <jtb6179@g.rit.edu>
2019-11-26 20:43:34 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,8 +1,8 @@
 def square_array(array)
   num_up = 0 
   
-  while num_up < array.length**2 do
-    puts array[num_up]    
+  while num_up < array.length do
+    puts array[num_up]**2    
     num_up += 1
   end
    
